1205b78c8004beaab2e7092fd3a16234aa7362d6
Sheeba-2505 <sreesheeba24@gmail.com>
2023-12-21 15:17:53 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserModule } from './user/user.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"admin",loadChildren:()=>import("./admin/admin.module").then(adm=>adm.AdminModule)},
+  {path:"user",loadChildren:()=>import("./user/user.module").then(use=>use.UserModule)}
+];
+
+console.log("App component module");
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/user/user-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {path:"list",component:ListComponent},
+  {path:"login",component:LoginComponent}
+];
+console.log("user Routed Module");
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UserRoutingModule { }
